fdd5fc2c9fb27755157f73e19a82306e19f7eb32
HenrikJuulsgaard <henrikpoulsen_87@hotmail.com>
2018-06-06 11:02:54 +0200

    Add project files.

---
+++ cs201806Kursus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs201806Kursus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
