7e466c2152b493a0c5d19d5856ddb9ec1a34a897
HenrikJuulsgaard <henrikpoulsen_87@hotmail.com>
2018-06-06 11:20:16 +0200

    Terning program added

---
+++ cs201806Kursus/cs201806 Terning.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs201806Kursus
+{
+    class Terning
+    {
+        public int værdi;
+        private bool snyd;
+        private static Random rnd = new Random();
+        private int Skriv()
+        {
+            return 2;
+        }
+        private int Ryst()
+        {
+            int tal;
+            if (!snyd)
+            {
+                tal = rnd.Next(1, 7);
+            }
+            else
+            {
+                tal = 6;
+            }
+            return tal;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
