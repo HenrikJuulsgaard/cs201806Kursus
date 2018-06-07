f8370d99c0c8b18efee5b3ca668d1db0d3004350
HenrikJuulsgaard <henrikpoulsen_87@hotmail.com>
2018-06-07 15:17:58 +0200

    Costum error handling

---
+++ cs201806 Costum Fejlhåndtering/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs201806_Costum_Fejlhåndtering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                StockItem Item1 = new StockItem(7);
+                Item1.ID = -7;
+
+            }
+            catch (StockItemException ex)
+            {
+                Console.WriteLine(" Stock Item error ");
+               
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(" General error ");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ cs201806 Costum Fejlhåndtering/StockItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs201806_Costum_Fejlhåndtering
+{
+    public class StockItem
+    {
+        public int ID { get; set; }
+
+        public StockItem(int id )
+        {
+            if (id < 0)
+            {
+                throw new StockItemException(" Wrong Id");
+            }
+        }
+    }
+
+    public class StockItemException : Exception
+    {
+        public StockItemException(string message) : base(message) { }
+    }
+}
