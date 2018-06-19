69b2b993dbe8f6a8ef3f97d2ce0eabb87377a3fb
HenrikJuulsgaard <henrikpoulsen_87@hotmail.com>
2018-06-19 15:25:16 +0200

    Swap program add

---
+++ cs201806 Generisk metode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs201806_Generisk_metode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 11;
+
+            Swap<int>(ref a, ref b);
+
+            Console.Read();
+        }
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ cs201806 collection kort/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs201806_collection_kort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bunke b = new Bunke();
+
+            b.Tilføj(new Kort { Kulør = "spar", Værdi = 8 });
+            b.Tilføj(new Kort { Kulør = "spar", Værdi = 7 });
+            b.Tilføj(new Kort { Kulør = "hjerter", Værdi = 9 });
+            b.Tilføj(new Kort { Kulør = "ruder", Værdi = 8 });
+
+            b.VisKort();
+
+            var k = b.FjernKort();
+            Console.WriteLine();
+            Console.WriteLine(k);
+
+            Console.Read();
+        }
+    }
+
+
+    public class Kort
+    {
+        public string Kulør { get; set; }
+        public int Værdi { get; set; }
+        public override string ToString()
+        {
+            return this.Kulør + " " +this.Værdi;
+        }
+    }
+
+    public class Bunke
+    {
+        private Stack<Kort> Korts = new Stack<Kort>();
+        public void Tilføj(Kort k)
+        {
+            this.Korts.Push(k);
+        }
+
+        public void VisKort()
+        {
+            foreach (var item in this.Korts)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public Kort FjernKort()
+        {
+            if (Korts.Count > 0)
+            {
+                return Korts.Pop();
+            }
+            return null;
+        }
+    }
+}
